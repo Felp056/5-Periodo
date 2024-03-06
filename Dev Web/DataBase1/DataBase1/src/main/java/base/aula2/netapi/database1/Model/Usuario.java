65f2d2be64918f0d57f9905881d824b199766888
Felipe Talini <felipetalini5@gmail.com>
2024-03-05 21:22:49 -0300

    Aula 2
    add nova pasta + aula de qa

---
+++ Dev Web/DataBase1/DataBase1/src/main/java/base/aula2/netapi/database1/Model/Usuario.java
@@ -0,0 +1,22 @@
+package base.aula2.netapi.database1.Model;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@Entity
+public class Usuario {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @Column(length = 60)
+    private String nome;
+    @Column(length = 20, nullable = false)
+    private String login;
+    @Column(nullable = false, length = 20)
+    private String senha;
+
+
+}
